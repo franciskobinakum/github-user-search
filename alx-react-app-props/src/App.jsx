4dfbaf01111f6eb3e53fd00ad0eb2d28e701344e
Francis Kobina Kum <kumfrancis.fk@gmail.com>
2026-01-18 01:38:26 +0000

    Refactor prop drilling to Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -12,6 +12,23 @@ function App() {
       <UserDetails />
     </UserContext.Provider>
   );
+}import UserContext from './context/UserContext';
+import UserProfile from './UserProfile';
+
+function App() {
+  const userData = {
+    name: "Jane Doe",
+    email: "jane.doe@example.com",
+  };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <UserProfile />
+    </UserContext.Provider>
+  );
 }
+
+export default App;
+
 
 export default App;
--- alx-react-app-props/src/context/UserContext.js
+++ alx-react-app-props/src/context/UserContext.js
@@ -1,5 +1,10 @@
 import { createContext } from 'react';
 
 const UserContext = createContext();
+import { createContext } from 'react';
+
+const UserContext = createContext();
+
+export default UserContext;
 
 export default UserContext;
