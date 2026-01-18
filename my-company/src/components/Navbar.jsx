5cb6927aa14876813e4918150bddd93f10d862bb
Francis Kobina Kum <kumfrancis.fk@gmail.com>
2026-01-18 02:13:18 +0000

    Build simple company website with React Router

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{
+      padding: '10px',
+      backgroundColor: '#0b74de',
+      display: 'flex',
+      gap: '15px'
+    }}>
+      <Link style={{ color: 'white' }} to="/">Home</Link>
+      <Link style={{ color: 'white' }} to="/about">About</Link>
+      <Link style={{ color: 'white' }} to="/services">Services</Link>
+      <Link style={{ color: 'white' }} to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
