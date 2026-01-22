2fbdf4ae46664d8e785dde7b927c50db1c852c86
Francis Kobina Kum <kumfrancis.fk@gmail.com>
2026-01-22 00:47:49 +0000

    Initialize Recipe Sharing App with Zustand

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,3 +4,7 @@ export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
